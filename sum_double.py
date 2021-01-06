315d7265f8423eb3ba23b4cdb922916320b305a1
nam-chu <74799165+nam-chu@users.noreply.github.com>
2021-01-06 15:49:45 -0500

    Delete sum_double.py

--- sum_double.py
+++
@@ -1,9 +0,0 @@
-# Given two int values, return their sum. Unless the two values are the same, then return double their sum.
-
-def sum_double(a, b):
-  sum = a + b
-  if a == b:
-    return 2*sum
-  else:
-    return sum
-
