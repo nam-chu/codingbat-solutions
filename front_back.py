745be6cd56c7c251bfa2daa53c83d6a8658720cd
nam-chu <74799165+nam-chu@users.noreply.github.com>
2021-01-06 15:48:30 -0500

    Delete front_back.py

--- front_back.py
+++
@@ -1,8 +0,0 @@
-# Given a string, return a new string where the first and last chars have been exchanged.
-def front_back(str):
-  if len(str) <= 1:
-    return str
-  mid = str[1:-1] # second index and second last index
-  return str[-1] + mid + str[0]
-  
- 
